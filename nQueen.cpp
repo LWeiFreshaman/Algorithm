573f108279413a676e40f566b7b52bc7fe0ffa60
lWWl <825454474@qq.com>
2019-04-04 12:16:33 +0800

    Add files via upload
    将回溯方法由递归修改为迭代，减少递归次数。

---
+++ nQueen.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+static int number = 0;
+vector<int> result;
+
+bool isValid(int q)
+{
+	for (int i = 0; i < result.size(); ++i)
+	{
+		if (q == result[i] || labs(result.size() - i) == labs(q - result[i]))
+			return false;
+	}
+	return true;
+}
+
+void _nQueen(int n)
+{
+	if (result.size() == n)
+	{
+		[] () {for (auto i : result ) { cout << i << " "; } cout << endl; }();
+		result.pop_back();
+		++number;
+		return;
+	}
+
+	for (int q = 0; q < n; ++q)
+	{
+		if (isValid(q))
+		{
+			result.push_back(q);
+			_nQueen(n);
+		}
+	}
+	result.pop_back();
+}
+
+int nQueen(int n)
+{
+	if (n < 2)
+		return 1;
+
+	_nQueen(n);
+
+	return number;
+}
+
+int main()
+{
+	cout << nQueen(12) << endl;
+
+	return 0;
+}
